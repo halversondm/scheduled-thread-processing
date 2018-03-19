d7f4945d06f9932587057aa6ab8ee59f55fcb13e
Dan Halverson <daniel.m.halverson@gmail.com>
2018-03-19 17:17:53 -0500

    initial commit

---
+++ src/main/java/com/halversondm/ScheduledThreadProcessingApplication.java
@@ -0,0 +1,12 @@
+package com.halversondm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduledThreadProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduledThreadProcessingApplication.class, args);
+	}
+}
